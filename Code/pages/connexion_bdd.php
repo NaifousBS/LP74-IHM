0b4f4c98b97163e7c45555fbde3e47fe1bda4104
Ahmet01100 <ahmet.imre@hotmail.fr>
2016-06-15 15:02:06 +0200

    Ajout bdd

---
+++ Code/pages/connexion_bdd.php
@@ -0,0 +1,21 @@
+<?php
+ /*
+ <?php
+require_once 'connexion_bdd.php';
+?>
+ */
+
+
+	$serveur ='localhost';
+	$user = 'root';
+	$pass = '';
+	$bdd ='pimpyourbody';
+try{
+	$connexion = new PDO ('mysql:host='.$serveur.';dbname='.$bdd, $user,$pass);
+    $connexion->exec("SET CHARACTER SET utf8");
+	}
+catch (PDOException $e)
+{
+	echo $e->getMessage();
+}
+?>
