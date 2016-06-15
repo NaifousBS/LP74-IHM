d8e8033a165ca7e0b574d94a23eff7bbe262951d
Ahmet01100 <ahmet.imre@hotmail.fr>
2016-06-15 15:34:10 +0200

    modif nom bdd

--- Code/pages/connexion_bdd.php
+++ Code/pages/connexion_bdd.php
@@ -9,7 +9,7 @@ require_once 'connexion_bdd.php';
 	$serveur ='localhost';
 	$user = 'root';
 	$pass = '';
-	$bdd ='pimpyourbody';
+	$bdd ='lp74_ihm';
 try{
 	$connexion = new PDO ('mysql:host='.$serveur.';dbname='.$bdd, $user,$pass);
     $connexion->exec("SET CHARACTER SET utf8");
