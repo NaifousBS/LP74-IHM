149341f3a1bdc0eabca6aaae0a6ff0d6eeffebd2
Ahmet01100 <ahmet.imre@hotmail.fr>
2016-06-20 23:32:36 +0200

    Correction du bug concernant la modif

--- Code/pages/renommerIshi.php
+++ Code/pages/renommerIshi.php
@@ -6,9 +6,14 @@ require_once 'connexion_bdd.php';
 
 <?php
 if(!empty($_POST)){
+    
+    echo $_POST['inputObjectifsSelect'].' '.$_POST['idObjetSelect'];
 
-$req = $connexion->prepare('UPDATE donnees SET CONTENU = $_POST['inputObjectifsSelect'] 
-	WHERE ID_DONNEES = $_POST['idObjetSelect']');
+    echo "UPDATE donnees SET CONTENU = '".$_POST['inputObjectifsSelect']."' 
+	WHERE ID_DONNEES =".$_POST['idObjetSelect'];
+    
+$req = $connexion->prepare("UPDATE donnees SET CONTENU = '".$_POST['inputObjectifsSelect']."' 
+	WHERE ID_DONNEES =".$_POST['idObjetSelect']);
     $req->execute();
 
 header('location:visualiser_ishikawa.php');
